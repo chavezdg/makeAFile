634a59a314f7e985b542bf411180024a08d7cd79
davidc <38849522+chavezdg@users.noreply.github.com>
2021-02-15 00:12:38 -0800

    Update makeAFile.sh

--- makeAFile.sh
+++ makeAFile.sh
@@ -1,21 +1,35 @@
+#/usr/bin/env bash
+
+davidc@mainm:~/tools$ cat makeAFile.sh 
 #!/usr/bin/env bash
 
 # Create a file with less steps.
 # Created by davidc
-# Release: 2/11/2021
+# Version 1.1
+# Release: 2/15/2021
 
 # Sometimes you want to retain any settings in your distro
 # and you do not wish to disturb the nature of your environment.
+# Also to have a header template made with less typing.
 
 currentUser=$( whoami )
+releaseDate=$( date "+%D" )
 echo ""
-read -p "ENTER FILE NAME: " fileName
+read -r -p "ENTER FILE NAME: " fileName
 echo "CREATING FILE NAME: $fileName"
 sudo touch "$fileName"
 echo "CHANGING OWNER OF $fileName TO $currentUser"
 sudo chown "$currentUser":"$currentUser" "$fileName"
 echo "CHANGING PERMISSIONS OF $fileName"
 chmod 755 "$fileName"
+case $fileName in
+ *".sh"*) echo '#/usr/bin/env bash' > "$fileName"
+	  echo "" >> "$fileName"
+	  echo "# BASH script $fileName" >> "$fileName"
+          echo "# Created by $currentUser" >> "$fileName"
+          echo "# Version 1" >> "$fileName"
+          echo "# Release: $releaseDate" >> "$fileName"
+	  ;;
+esac
 echo "FILE $fileName IS IN $PWD"
 ls -l "$fileName"; echo ""
-
